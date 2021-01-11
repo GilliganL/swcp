5ef38b63c44a45bfd352c4f9b2b4146032c6cbe0
GilliganL <lynseympowell@gmail.com>
2021-01-10 18:58:26 -0700

    Fix 404 sytle

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1 style={{ width: '100%' }}>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist...</p>
+    <div style={{ width: '100%' }}>
+      <h1 style={{ width: '100%' }}>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist...</p>
+    </div>
   </Layout>
 )
 
