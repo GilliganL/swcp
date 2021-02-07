04f7794740c252a58eefbbd36f0a7031299bb440
GilliganL <lynseympowell@gmail.com>
2021-02-06 18:56:35 -0700

    Add info to footer

--- src/components/footer/view.js
+++ src/components/footer/view.js
@@ -6,6 +6,12 @@ const FooterView = () => (
   <footer className={styles.footer}>
     <div className={styles.footerContent}>
       © {new Date().getFullYear()}
+      <a className={styles.value} href="mailto:scparts@comcast.net">
+        scparts@comcast.net
+      </a>
+      <a className={styles.value} href="tel:505-220-4076">
+        505-220-4076
+      </a>
       <a href="tel:505-220-4076" className={styles.footerText}>
         Call us to order
         <RightArrow className={styles.footerText__arrow} />
